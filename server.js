1dfc08505cc73e47ef0dc4a2dbce4b77262ead4c
Madhav Khurana <madhavkhurana086@gmail.com>
2019-11-24 15:17:52 -0600

    ab

--- server.js
+++ server.js
@@ -43,13 +43,13 @@ app.use("/api/admin", admin);
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 const port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
 
-if (port === process.env.OPENSHIFT_NODEJS_PORT) {
-  app.use(express.static("client/build"));
+// if (port === process.env.OPENSHIFT_NODEJS_PORT) {
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+// }
 app.listen(port, server_ip_address, () =>
-  console.log(`Running on port ${port}`)
+  console.log(`Running on port ${port} and ${server_ip_address}`)
 );
