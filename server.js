5c2421f0c67635c0cb61882815a0667f64a7d381
Madhav Khurana <madhavkhurana086@gmail.com>
2019-11-24 22:57:31 -0600

    AWS

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.use("/api/users", users);
 app.use("/api/pdf", pdf);
 app.use("/api/admin", admin);
 
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-const port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+const port = process.env.PORT || 5000;
 
 // if (port === process.env.OPENSHIFT_NODEJS_PORT) {
 app.use(express.static("client/build"));
@@ -50,6 +50,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 // }
-app.listen(port, server_ip_address, () =>
+app.listen(port, () =>
   console.log(`Running on port ${port} and ${server_ip_address}`)
 );
